ab26ef1ad317734f564dcee7df5c7c131b466c45
amiteshksharma <amitesh.sharma.2001@gmail.com>
2020-04-12 22:25:51 -0700

    home page redesign part 1

--- college-app/src/Home.js
+++ college-app/src/Home.js
@@ -7,16 +7,23 @@ function Home() {
     return (
         <div className="Background-home-page">
             <Nav />
-            <div className="slogan">
-                <h1 className="slogan-text">All your needs, organized. One Place.</h1>
-            </div>
-
             <div className="searchbar">
                 <input className="bar" type="text" placeholder="Find your dream school" />
 
-                <div className="advanced">
-                    Advanced
+                <div className="advanced-box">
+                    <h2 className="advanced">Advanced</h2>
                 </div>
+            </div>
+
+            <div className="slogan-image">
+
+                <div className="slogan">
+                    <h1 className="slogan-text">All your needs, organized. One Place.</h1>
+                </div>
+                <div className="description-website">
+                    <p className="description">Here is a filling paragraph dasd asdas asdas dasd asdas </p>
+                </div>
+
             </div>
         </div>
     );
