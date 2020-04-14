b548c68773f28b96cb49376426ee2604ec50f6a5
amiteshksharma <amitesh.sharma.2001@gmail.com>
2020-04-13 21:28:32 -0700

    redesigned home page

--- college-app/src/Home.js
+++ college-app/src/Home.js
@@ -2,13 +2,20 @@ import React from 'react';
 import './Home.css';
 
 import Nav from './Nav';
+import Image from './Student_picture.png';
 
 function Home() {
     return (
         <div className="Background-home-page">
             <Nav />
+            <span>
+                <h1 className="logo-header">All your needs, organized. One Place.</h1>
+            </span>
             <div className="searchbar">
-                <input className="bar" type="text" placeholder="Find your dream school" />
+                <form className="search-bar">
+                    <input className="bar" type="text" placeholder="Find your dream school" />
+                    <button className="search-button" type="submit">Search</button>
+                </form>
 
                 <div className="advanced-box">
                     <h2 className="advanced">Advanced</h2>
@@ -16,14 +23,18 @@ function Home() {
             </div>
 
             <div className="slogan-image">
-
-                <div className="slogan">
-                    <h1 className="slogan-text">All your needs, organized. One Place.</h1>
+                <div className="description-slogan">
+                    <span className="slogan">
+                        <h1 className="slogan-text">College, Simplified</h1>
+                    </span>
+                    <div className="description-website">
+                        <p className="description">Time to simplify college searching. Welcome to College Organizer, a place where
+                        you can find, compare, and determine your dream college.</p>
+                    </div>
                 </div>
-                <div className="description-website">
-                    <p className="description">Here is a filling paragraph dasd asdas asdas dasd asdas </p>
+                <div className="vector-image">
+                    <img src={Image} height="500"></img>
                 </div>
-
             </div>
         </div>
     );
