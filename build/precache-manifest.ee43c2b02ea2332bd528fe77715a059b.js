fa3e79b5b80974d6a4b71a3b13f23d194ce2c57b
Mihir Gupta <m08.in.us@gmail.com>
2020-06-30 17:42:13 -0700

    moved everything up 1 level

---
+++ build/precache-manifest.ee43c2b02ea2332bd528fe77715a059b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8abc889d66112293280ac7f90b474033",
+    "url": "/index.html"
+  },
+  {
+    "revision": "00a75c4f6a019da6b9d0",
+    "url": "/static/css/main.846739cd.chunk.css"
+  },
+  {
+    "revision": "dec3aa8e07892499ea3a",
+    "url": "/static/js/2.45f4be41.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.45f4be41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00a75c4f6a019da6b9d0",
+    "url": "/static/js/main.fd21aa8d.chunk.js"
+  },
+  {
+    "revision": "f941cc7c7b48aeb43b2b",
+    "url": "/static/js/runtime-main.5a0076cc.js"
+  },
+  {
+    "revision": "a3ab709080b51cc20821ca719215b78d",
+    "url": "/static/media/Student_picture.a3ab7090.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "e0323fc4bbb26421eff887a6de707ff6",
+    "url": "/static/media/stephen_the_goat.e0323fc4.jpg"
+  }
+]);
