bdff48ba58fb9e49a1e0d613bd2ddc5d16978008
Mihir Gupta <m08.in.us@gmail.com>
2020-06-30 17:51:41 -0700

    added procfile and gunicorn

--- api/api.py
+++ api/api.py
@@ -7,6 +7,9 @@ from flask import Flask
 app = Flask(__name__, static_folder='../build', static_url_path='/')
 
 
+if __name__ == '__main__':
+    app.run(debug=False)
+
 
 @app.route('/')
 def index():
