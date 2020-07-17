faa72a869616ccd93992e355c97fb42e3a7dd812
nvanny <nvanny@ucsd.edu>
2020-07-17 04:56:50 -0700

    map and basic pie chart(will be updated later)

---
+++ src/components/MapComponent.js
@@ -0,0 +1,35 @@
+import { Map, GoogleApiWrapper, InfoWindow, Marker } from 'google-maps-react';
+import React, { Component } from 'react';
+
+const { REACT_APP_API_KEY } = process.env;
+
+const mapStyles = {
+    position: 'static',
+    width: '100%',
+    height: '100%'
+  };
+
+export class MapComponent extends Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+      return (
+        <Map
+          google={this.props.google}
+          zoom={14}
+          style={mapStyles}
+          initialCenter={{
+           lat: this.props.lat, 
+           lng: this.props.lng
+          }}
+        >
+            <Marker />
+        </Map>
+      );
+    }
+  }
+ 
+export default GoogleApiWrapper({
+    apiKey: REACT_APP_API_KEY
+})(MapComponent);
