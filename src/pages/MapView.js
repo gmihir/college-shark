ce0668a84f7ce833fc6e07b03d834fa8a7364b15
Amitesh Sharma <amitesh.sharma.2001@gmail.com>
2020-07-18 15:58:22 -0700

    mapview almost complete, will work on searchbar for map view page tomorrow

--- src/pages/MapView.js
+++ src/pages/MapView.js
@@ -28,12 +28,22 @@ export class MapView extends Component {
           CollegeMap: [],
           isVisible: false,
           activeMarker: {},
-          selectedPlace: {}
+          selectedPlace: {},
+          Bounds: {
+            lat: 40.854885,
+            lng: -98.081807
+          },
+          Zoom: 5,
+          ShowReset: false
       }
 
         this.searchBarInUse = this.searchBarInUse.bind(this);
         this.setSearch = this.setSearch.bind(this);
         this.markerClicked = this.markerClicked.bind(this);
+        this.resetMap = this.resetMap.bind(this);
+        this.numFormat = this.numFormat.bind(this);
+        this.dateFormat = this.dateFormat.bind(this);
+        this.rankFormat = this.rankFormat.bind(this);
     }
 
     searchBarInUse = (inUse) => {
@@ -50,19 +60,103 @@ export class MapView extends Component {
       }
     }
 
-    markerClicked(props, marker) {
-      console.log("here");
-      console.log(props)
-      console.log(marker);
+    rankFormat(num) {
+      if(num === null) {
+        return num
+      } else if(num >= 10000) {
+        return "Unranked"
+      } else {
+        return num;
+      }
+    }
+
+    numFormat(num) {
+      if (num === null) {
+          return num;
+      } else {
+          return num.toLocaleString();
+      }
+    }
+
+    dateFormat(input) {
+      if(input === -1) {
+          return("N/A");
+      }
+      else if(input === 0) {
+        return("Rolling");
+      }
+      var myDate = new Date(input * 1000);
+      return ((myDate.getUTCMonth() + 1) + "/" + myDate.getUTCDate() + "/" + myDate.getUTCFullYear());
+    }
+    
+    resetMap() {
       this.setState({
-        selectedPlace: props,
+        Bounds: {
+          lat: 40.854885,
+          lng: -98.081807
+        },
+        Zoom: 5,
+        ShowReset: false  
+      })
+    }
+
+    markerClicked(props, marker) {
+      //Create an object
+      let object = {};
+
+      //Retrieve only the data necessary from props
+      //Which is college details, name, and title
+      object.college = JSON.parse(props.college);
+      object.name = props.name;
+      object.title = props.title;
+      
+      //Set the state accordingly
+      this.setState({
+        selectedPlace: object,
         activeMarker: marker,
-        isVisible: true
+        isVisible: true,
+        Zoom: 12,
+        ShowReset: true
+      }, () => {
+        //Set the sessionStorage for when user comes back to the page
+        const bound = {
+          lat: this.state.selectedPlace.college.latitude,
+          lng: this.state.selectedPlace.college.longitude
+        }
+        this.setState({Bounds: bound})
+        sessionStorage.setItem('mapview', JSON.stringify(this.state.selectedPlace.college));
+        sessionStorage.setItem('mapviewName', JSON.stringify(this.state.selectedPlace.name));
       })
     }
 
     componentDidMount() {
       console.log("here: fetch2")
+      if(sessionStorage.getItem('mapview') === '') {
+        //Check to see sessionStorage is empty
+      } else {
+        //Get items out of sessionStorage, only need the name and details, no title
+        const toArray = JSON.parse(sessionStorage.getItem('mapview'));
+        const getName = JSON.parse(sessionStorage.getItem('mapviewName'));
+        
+        //Create an object to resemble that in this.markerClicked()
+        let obj = {};
+        obj.college = toArray;
+        obj.name = getName;
+
+        this.setState({
+          selectedPlace: obj, 
+          isVisible: true,
+          Zoom: 12,
+          Bounds: {
+            lat: obj.college.latitude,
+            lng: obj.college.longitude
+          },
+          ShowReset: true
+        }, () => {
+          console.log(this.state.selectedPlace);
+        });
+      }
+
       fetch("/searchbar", {
         method: "POST",
         headers: {
@@ -106,14 +200,15 @@ export class MapView extends Component {
             <NavBar searchBarInUse={this.searchBarInUse} setSearch={this.setSearch} searchBar={this.state.searchBar} active="4" 
             location={this.props.location} />
             <section className="mapview">
+              {this.state.ShowReset ? 
+              <div className="reset-map" onClick={() => this.resetMap()}>
+                <button className="reset-button">Reset Mapview</button>
+              </div> : null}
                 <Map
                     google={this.props.google}
-                    zoom={5}
+                    zoom={this.state.Zoom}
                     style={mapStyles}
-                    initialCenter={{
-                        lat: 40.854885,
-                        lng: -98.081807
-                    }}
+                    center={this.state.Bounds}
                 >
                     {this.state.CollegeMap.map(college => {
                       const lat = JSON.parse(college).latitude;
@@ -134,6 +229,7 @@ export class MapView extends Component {
                     />
                 </div>
             </section> : 
+            /* If a college is selected, then this will render */
               <section className="college-display">
                 <div className="map-search">
                     <SearchBar searchBarInUse={this.searchBarInUse} setSearch={this.setSearch} list={this.state.Colleges} 
@@ -142,26 +238,31 @@ export class MapView extends Component {
                 </div>
 
                 <section className="img-section">
-                    <img src={JSON.parse(this.state.selectedPlace.college).college_campus} alt="College Campus" width="100%" height="100%" />
+                    <img src={this.state.selectedPlace.college.college_campus} alt="College Campus" width="100%" height="100%" />
                 </section>
                 
                 <div className="college-information">
                     <h1 className="college-name">{this.state.selectedPlace.name}</h1>
+                    <h3>Ranking: {this.rankFormat(this.state.selectedPlace.college.national_ranking)}</h3>
                     <hr></hr>
 
                     <div>
-                        <button className="individual-redirect">FIND OUT MORE NEXT TIME ON MASTER CHEF</button>
+                        <button className="individual-redirect" 
+                        onClick={() => this.props.history.push(`/loginhome/page/${this.state.selectedPlace.name}`)}>
+                          Click here to learn more
+                        </button>
                     </div>
                     
-                    <hr></hr>
+                    <div className="heart-location">
+                      <hr></hr>
 
-                    <div className="college-add-icon">
-                        <Heart />
+                      <div className="college-add-icon">
+                          <Heart collegeName={this.state.selectedPlace.name} key={this.state.selectedPlace.name} />
+                      </div>
                     </div>
                     
                     <div className="college-info-container">
                         <div className="college-info-header">
-                            <h2>Ranking:</h2>
                             <h2>Tuition:</h2>
                             <h2>State:</h2>
                             <h2>Town type:</h2>
@@ -170,12 +271,11 @@ export class MapView extends Component {
                         </div>
 
                         <div className="college-info-values">
-                            <h2>{JSON.parse(this.state.selectedPlace.college).national_ranking}</h2>
-                            <h2>{JSON.parse(this.state.selectedPlace.college).tuition_normal}</h2>
-                            <h2>{JSON.parse(this.state.selectedPlace.college).state}</h2>
-                            <h2>{JSON.parse(this.state.selectedPlace.college).locale}</h2>
-                            <h2>{JSON.parse(this.state.selectedPlace.college).population}</h2>
-                            <h2>{JSON.parse(this.state.selectedPlace.college).regular_decision}</h2>   
+                            <h2>${this.numFormat(this.state.selectedPlace.college.tuition_normal)}</h2>
+                            <h2>{this.state.selectedPlace.college.state}</h2>
+                            <h2>{this.state.selectedPlace.college.locale}</h2>
+                            <h2>{this.numFormat(this.state.selectedPlace.college.population)}</h2>
+                            <h2>{this.dateFormat(this.state.selectedPlace.college.regular_decision)}</h2>   
                         </div>
                     </div>
 
