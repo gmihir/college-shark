d0de44501f197c7f86d77fe7771c44da07fb1070
Mihir Gupta <m08.in.us@gmail.com>
2020-07-17 19:06:52 -0700

    updating map to include state

--- main.py
+++ main.py
@@ -52,7 +52,7 @@ headers_individual = ["college_name","transcripts","mid_year","letter_of_rec_req
 headers_searchbar = ["college_name","alias","abbreviation","college_logo"]
 
 headers_map = ["college_name","population","national_ranking","tuition_normal","tuition_oos",
-                "regular_decision","college_logo","college_campus", "latitude","longitude""locale"]
+                "regular_decision","state","college_logo","college_campus", "latitude","longitude""locale"]
 
 app = flask.Flask(__name__, static_folder='./build', template_folder = "./build", static_url_path='/')
 CORS(app)
