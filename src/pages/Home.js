6251bc967ce79323c897ce0fe623df0ee0ed9ae2
Amitesh Sharma <amitesh.sharma.2001@gmail.com>
2020-08-09 15:55:22 -0700

    mobile design done for essays and home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -106,19 +106,11 @@ function Home() {
             <div className="image-background">
                 <Container style={{paddingBottom: 'calc(5vh)'}}>
                     <Row className="justify-content-md-center">
-                        <Col md="auto" style={{fontSize: "calc(5.5rem"}}>100 APPS. ONE PLACE.</Col>
+                        <Col md="auto" style={{fontSize: window.innerWidth <= 760 ? 'calc(11vw)' : "calc(5vw)" }}>100 APPS. ONE PLACE.</Col>
                     </Row>
 
                     <Row className="justify-content-md-center">
-                        <Col md="auto" style={{fontSize: "calc(2rem", marginTop: 'calc(0.7rem)'}}>College Apps, Redefined.</Col>
-                    </Row>
-
-                    <Row className="justify-content-md-center">
-                        {/* <Link to="/signup">
-                            <Button variant="primary" size="lg">
-                                Sign up
-                            </Button>{' '}
-                        </Link> */}
+                        <Col md="auto" style={{fontSize: window.innerWidth <= 760 ? 'calc(5vw)' : "calc(2.2vw)", marginTop: 'calc(0.7vw)'}}>College Apps, Redefined.</Col>
                     </Row>
                 </Container>
                 
@@ -128,7 +120,6 @@ function Home() {
                         <SearchBar searchBarInUse={searchBarInUse} setSearch={setSearch} list={colleges.Colleges} />
                     </div>
                 </div>
-
             </div>
 
             <Carousel pause={true} interval={3500}>
@@ -175,36 +166,13 @@ function Home() {
             <div className="features-container">
                 <div className="features">
                     <h1 className="header-h1">FEATURES</h1>
-                    
-                    {/* <div className="header-div">
-                        <h1 className="header-feature">SEARCH</h1>
-                        <h1 className="header-feature">EXPLORE</h1>
-                        <h1 className="header-feature">ORGANIZE</h1>
-                    </div> */}
 
-                    <Container fluid style={{marginTop: 'calc(-7vh)', marginBottom: 'calc(-5vh)'}}>
+                    <Container>
                         <Row>
-                            <Col xs={6} md={4}>
+                            <Col xs={12} md={4} style={{marginTop: 'calc(-5vh)'}}>
                                 <div style={{display: 'flex', justifyContent: 'center'}}>
                                     <h1 className="header-feature">ESSAYS</h1>
                                 </div>
-                            </Col>
-                            <Col xs={6} md={4}>
-                                <div style={{display: 'flex', justifyContent: 'center'}}>
-                                    <h1 className="header-feature">EXPLORE</h1>
-                                </div>
-                            </Col>
-                            <Col xs={6} md={4}> 
-                                <div style={{display: 'flex', justifyContent: 'center'}}>
-                                    <h1 className="header-feature">ORGANIZE</h1>
-                                </div>
-                            </Col>
-                        </Row>
-                    </Container>
-
-                    <Container fluid>
-                        <Row>
-                            <Col xs={6} md={4}>
                                 <div className='description-hover'>
                                     <div className="icon-border">
                                         <div><FontAwesomeIcon icon={faScroll} /></div>
@@ -212,7 +180,10 @@ function Home() {
                                     <p>Organize all of your essay requirements in one place. Optimize your workflow and work smarter, not harder.</p>
                                 </div>
                             </Col>
-                            <Col xs={6} md={4}>
+                            <Col xs={12} md={4}>
+                                <div style={{display: 'flex', justifyContent: 'center'}}>
+                                    <h1 className="header-feature">EXPLORE</h1>
+                                </div>
                                 <div className='description-hover'>
                                     <div className="icon-border">
                                         <div><FontAwesomeIcon icon={faGlobeAmericas} /></div>
@@ -221,7 +192,10 @@ function Home() {
                                     find hundreds of colleges that fit your needs. </p>
                                 </div>
                             </Col>
-                            <Col xs={6} md={4}> 
+                            <Col xs={12} md={4}> 
+                                <div style={{display: 'flex', justifyContent: 'center'}}>
+                                    <h1 className="header-feature">ORGANIZE</h1>
+                                </div>
                                 <div className='description-hover'>
                                     <div className="icon-border">
                                         <div><FontAwesomeIcon icon={faLayerGroup} /></div>
@@ -239,20 +213,22 @@ function Home() {
                 <h1>Questions? Contact Us.</h1>
                     <Form>
                         <Form.Row>
-                            <Col style={{marginTop: 'calc(4rem)'}}>
+                            <Col xs={{ span: 10, offset: 1}} md={{ span: 6, offset: 0}} style={{marginTop: window.innerWidth <= 760 ? 'calc(1rem)' : 'calc(4rem)'}}>
                                 <Form.Group controlId="exampleForm.ControlInput1" style={{display: 'flex', justifyContent: 'flex-end'}}>
-                                    <Form.Control type="text" placeholder="John Doe" style={{width: 'calc(55%)'}} onChange={(e) => setName({Name: e.target.value})} 
+                                    <Form.Control type="text" placeholder="John Doe" style={{width: window.innerWidth <= 760 ? 'calc(100%)' : 'calc(55%)'}} 
+                                    onChange={(e) => setName({Name: e.target.value})} 
                                         value={name.Name}
                                     />
                                 </Form.Group>
                                 <Form.Group controlId="exampleForm.ControlInput1" style={{display: 'flex', justifyContent: 'flex-end'}}>
-                                    <Form.Control type="email" placeholder="name@example.com" style={{width: 'calc(55%)'}} 
+                                    <Form.Control type="email" placeholder="name@example.com" style={{width: window.innerWidth <= 760 ? 'calc(100%)' : 'calc(55%)'}} 
                                     onChange={(e) => setEmail({Email: e.target.value})} value={email.Email}/>
                                 </Form.Group>
                             </Col>
-                            <Col style={{marginTop: 'calc(4rem)'}}>
+                            <Col xs={{ span: 10, offset: 1}} md={{ span: 6, offset: 0}} style={{marginTop: window.innerWidth <= 760 ? 'calc(1.5rem)' : 'calc(4rem)'}}>
                                 <Form.Group controlId="exampleForm.ControlTextarea1" style={{display: 'flex', justifyContent: 'left'}}>
-                                    <Form.Control as="textarea" rows="3" style={{height: 'calc(10rem)', width: 'calc(55%)'}} placeholder="Enter message here..."
+                                    <Form.Control as="textarea" rows="3" style={{height: 'calc(10rem)', width: window.innerWidth <= 760 ? 'calc(100%)' : 'calc(55%)'}} 
+                                    placeholder="Enter message here..."
                                         onChange={(e) => setMessage({Message: e.target.value})} value={message.Message}
                                     />
                                 </Form.Group>
